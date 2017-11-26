55e3ec6ef7d522a146bf0c54e166da85e1afa347
GrimNew <326408489@qq.com>
2017-11-26 15:50:37 +0800

    Initial commit

---
+++ src/MonitorServer.java
@@ -0,0 +1,84 @@
+//导入外部jar包
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Grim on 2017-02-17.
+ */
+public class MonitorServer {
+    private static final String url=
+            "jdbc:mysql://localhost:3306/monitor" +//JDBC方式/MySQL数据库/本机/端口3306/数据库名称
+                    "?useSSL=false&useUnicode=true&characterEncoding=utf8";//SSL关闭/使用Unicode编码/编码方式utf-8
+    private static final String user="root";
+    private static final String password="root";
+
+    public static void main(String[] args) {
+        //创建线程池等待任务队列，数量10
+        BlockingQueue<Runnable> blockingQueue=new ArrayBlockingQueue<>(10);
+        //创建线程池，基本线程5个，最大20个，存活时间2分钟，源自于等待任务队列
+        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(
+                5,20,2, TimeUnit.MINUTES ,blockingQueue);
+
+        //创建对象
+        Connection connection =null;
+        ServerSocket serverSocket=null;
+        Socket socket=null;
+        Statement statement=null;
+
+        try {
+            //反射加载驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            //连接数据库对象实例化
+            connection = DriverManager.getConnection(url,user,password);
+            if (connection != null){
+                System.out.println("连接成功！");
+            }
+            //获取statement数据库操作实例
+            if (connection != null) {
+                statement= connection.createStatement();
+            }
+            //serverSocket实例绑定6000端口
+            serverSocket = new ServerSocket(60000);
+
+            while (true) {
+                for (int i=0;i<30;i++) {
+                    //启动侦听并进入阻塞状态
+                    socket = serverSocket.accept();
+                    //启动线程
+                    threadPoolExecutor.execute(new MonitorServerThread("thread" + (i + 1), statement, socket));
+                }
+            }
+
+        } catch (ClassNotFoundException | SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭相关资源
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+                threadPoolExecutor.shutdown();
+            } catch (IOException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/MonitorServerThread.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Grim on 2017-02-19.
+ */
+
+//创建单线程类，继承Runnable接口
+public class MonitorServerThread implements Runnable{
+
+    //创建对象
+    private String threadName;
+    private Statement statement;
+    private Socket socket;
+
+    //构造函数完成初始化类
+    MonitorServerThread(String threadName, Statement statement, Socket socket){
+        this.threadName=threadName;
+        this.statement=statement;
+        this.socket=socket;
+    }
+
+    /*
+    * 重写run()方法，完成线程功能
+    * */
+    @Override
+    public void run(){
+
+        String T_SQL;   //T-SQL语句字符串
+        String[] splitMessage;  //存储拆分消息字符串数组
+        try {
+            InputStream inputStream = socket.getInputStream();//创建输入流对象
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);//读取输入流，注：一次读取一行，发送时必须要换行
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);//缓存输入流
+            String message; //消息字符串
+            while ((message=bufferedReader.readLine())!=null){
+                System.out.println(message);//输出接收到的原消息
+                splitMessage =  message.split("#"); //调用split()方法以“#”拆分若干段
+                T_SQL="INSERT INTO data_table(device_ID,digital,analog) VALUES('"
+                        +splitMessage[0]+"','"+splitMessage[1]+"','"+splitMessage[2]+"')";  //拼接T-SQL语句
+                if(statement.executeUpdate(T_SQL)!=0){  //执行T-SQL语句，执行成功返回不为NULL
+                    System.out.println(threadName+ "成功添加！");
+                }
+            }
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close(); //关闭socket资源
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
